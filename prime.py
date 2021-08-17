0c1afb3c645756b2de134732cc022f6b93bba221
surendhargit <89027681+surendhargit@users.noreply.github.com>
2021-08-17 12:23:42 -0400

    Add files via upload
    prime number find

---
+++ prime.py
@@ -0,0 +1,8 @@
+num= int(input("enter the number"))
+for i in range(2,num):
+     if num%2==0:
+         print(" it is not prime number")
+         break
+else:
+        print("prime number")
+              
